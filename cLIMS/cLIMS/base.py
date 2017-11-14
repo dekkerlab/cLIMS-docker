00e2953a9cd829275304be2b817902dbdde134a4
nandankita <nandankita@gmail.com>
2017-11-14 14:54:51 -0500

    Update base.py

--- cLIMS/cLIMS/base.py
+++ cLIMS/cLIMS/base.py
@@ -4,6 +4,7 @@ Created on Oct 7, 2016
 @author: nanda
 '''
 
+ROOTFOLDER = "/cLIMS"
 WORKSPACEPATH = "/home/ubuntu/clims-docker/cLIMS/"
 LABNAME = 'dekker-lab:'
 FILEUPLOADPATH = "/home/ubuntu/media/"
